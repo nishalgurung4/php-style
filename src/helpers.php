87e58f437018c5e35ea6470940aa3b83df32770c
Nishal Gurung <nishal.gurung4@outlook.com>
2021-05-17 17:51:41 +0545

    fix static method issue

--- src/helpers.php
+++ src/helpers.php
@@ -8,7 +8,8 @@ use PhpCsFixer\Finder;
 function styles(Finder $finder, array $rules = []): Config {
   $rules = array_merge(require __DIR__.'/rules.php', $rules);
 
-  return Config::create()
+  $config = new Config();
+  return $config
     ->setFinder($finder)
     ->setRiskyAllowed(true)
     ->setRules($rules);
